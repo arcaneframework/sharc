d6ddb632d4846997ffa18e67ef142894f6014d05
guignont <thomas.guignon@ifpen.fr>
2024-11-14 10:49:48 +0100

    - fix formating

--- common/ArcGeoSim/src/ArcGeoSim/Numerics/ShArcTools/Solver/Newton.h
+++ common/ArcGeoSim/src/ArcGeoSim/Numerics/ShArcTools/Solver/Newton.h
@@ -1,6 +1,6 @@
 // -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
-// Copyright 2000-2022 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// Copyright 2000-2024 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
@@ -66,15 +66,7 @@
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-namespace ArcNum {
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-
-namespace Algorithm {
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
+namespace ArcNum::Algorithm {
 
   template<typename TemplateObject>
   class Newton
@@ -182,11 +174,10 @@ namespace Algorithm {
       bool NonPhysicalSolution = false;
       m_object.newtonUpdate(NonPhysicalSolution);
       if(NonPhysicalSolution){
-	    if(m_verbose)
-	      info() << " *** Newton error : non physical solution";
-
-      //return m_results.failed(Results::NonPhysicalSolution);
-      return false;
+        if(m_verbose)
+          info() << " *** Newton error : non physical solution";
+        //return m_results.failed(Results::NonPhysicalSolution);
+        return false;
       }
 
       m_object.newtonLinearize();
@@ -228,14 +219,6 @@ namespace Algorithm {
 
     return false;
   }
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-
-}
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
 
 }
 
