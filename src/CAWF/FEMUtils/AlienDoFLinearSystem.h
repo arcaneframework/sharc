83c998976e3217ed4e92cf148e6908de79102999
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2026-03-18 09:12:45 +0100

    add Elasticity module enable system assembly on Accelerator

---
+++ src/CAWF/FEMUtils/AlienDoFLinearSystem.h
@@ -0,0 +1,216 @@
+// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+//-----------------------------------------------------------------------------
+// Copyright 2000-2025 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: Apache-2.0
+//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
+/* PETScDoFLinearSystem.cc                                     (C) 2022-2025 */
+/*                                                                           */
+/* Linear system: Matrix A + Vector x + Vector b for Ax=b.                   */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+#pragma once
+
+#include "DoFLinearSystem.h"
+
+#include <mpi.h>
+#include <arccore/trace/TraceAccessor.h>
+
+#include <arcane/accelerator/RunCommandLoop.h>
+#include <arcane/utils/FatalErrorException.h>
+#include <arcane/utils/PlatformUtils.h>
+#include <arcane/utils/ArcaneGlobal.h>
+#include <arcane/utils/MemoryUtils.h>
+#include <arcane/utils/MemoryView.h>
+#include <arcane/utils/ITraceMng.h>
+#include <arcane/utils/NumArray.h>
+#include <arcane/utils/CommandLineArguments.h>
+
+#include <arcane/core/ServiceFactory.h>
+#include <arcane/core/VariableTypes.h>
+#include <arcane/core/BasicService.h>
+#include <arcane/core/IParallelMng.h>
+#include <arcane/core/IItemFamily.h>
+#include <arcane/core/ItemPrinter.h>
+#include <arcane/core/Timer.h>
+
+#include <arcane/accelerator/VariableViews.h>
+#include <arcane/accelerator/core/Runner.h>
+#include <arcane/accelerator/core/Memory.h>
+
+#include "IDoFLinearSystemFactory.h"
+
+#include <alien/arcane_tools/accessors/ItemVectorAccessor.h>
+#include <alien/core/block/VBlock.h>
+
+#include <alien/arcane_tools/IIndexManager.h>
+#include <alien/arcane_tools/indexManager/BasicIndexManager.h>
+#include <alien/arcane_tools/indexManager/SimpleAbstractFamily.h>
+#include <alien/arcane_tools/distribution/DistributionFabric.h>
+#include <alien/arcane_tools/indexSet/IndexSetFabric.h>
+#include <alien/arcane_tools/data/Space.h>
+
+#include <alien/kernels/simple_csr/algebra/SimpleCSRLinearAlgebra.h>
+#include <alien/kernels/simple_csr/algebra/SimpleCSRInternalLinearAlgebra.h>
+
+#include <alien/ref/AlienRefSemantic.h>
+#include <alien/ref/AlienImportExport.h>
+
+#include <alien/kernels/redistributor/Redistributor.h>
+#include <alien/ref/data/scalar/RedistributedVector.h>
+#include <alien/ref/data/scalar/RedistributedMatrix.h>
+#include <alien/ref/import_export/MatrixMarketSystemWriter.h>
+
+#include <alien/expression/solver/SolverStater.h>
+#include <alien/AlienLegacyConfig.h>
+
+#include <AlienDoFLinearSystemFactory_axl.h>
+
+#ifdef ALIEN_USE_PETSC
+#include <alien/kernels/petsc/io/AsciiDumper.h>
+#include <alien/kernels/petsc/algebra/PETScLinearAlgebra.h>
+#endif
+
+#ifdef ALIEN_USE_HYPRE
+#include <alien/kernels/hypre/HypreBackEnd.h>
+#include <alien/kernels/hypre/data_structure/HypreMatrix.h>
+#include <alien/kernels/hypre/data_structure/HypreVector.h>
+#include <alien/kernels/hypre/algebra/HypreLinearAlgebra.h>
+#endif
+
+#include "CAWF/FEMUtils/internal/CsrDoFLinearSystemImpl.h"
+#include "CAWF/FEMUtils/AlienBSRFormat.h"
+
+namespace Arcane::FemUtils
+{
+
+using namespace Arcane;
+using namespace Alien;
+
+class AlienDoFLinearSystemImpl
+//: public CsrDoFLinearSystemImpl
+: public DoFLinearSystemImplBase
+{
+ public:
+
+  AlienDoFLinearSystemImpl(IItemFamily* dof_family, const String& solver_name) ;
+
+  ~AlienDoFLinearSystemImpl() override ;
+
+ public:
+
+  AlienBSRFormat* bsr(ITraceMng* trace_mng, RunQueue& queue, FemDoFsOnNodes& dofs_on_nodes)
+  {
+    if(not m_alien_bsr_format.get())
+      m_alien_bsr_format.reset( new AlienBSRFormat{trace_mng,queue,dofs_on_nodes,options->useAccelerator()}) ;
+    return m_alien_bsr_format.get();
+  }
+
+  Int32 indexValue(DoFLocalId row_lid, DoFLocalId column_lid)
+  {
+    /*
+    auto begin = m_csr_view.rows()[row_lid];
+    auto end = row_lid == m_csr_view.nbRow() - 1 ? m_csr_view.nbColumn() : m_csr_view.row(row_lid + 1);
+    for (auto i = begin; i < end; ++i)
+      if (m_csr_view.columns()[i] == column_lid)
+        return i;
+    */
+    return -1;
+  }
+
+ public:
+
+  void matrixAddValue(DoFLocalId row, DoFLocalId column, Real value) override
+  {
+    //m_csr_view.values()[indexValue(row, column)] += value;
+    (*m_builder)(m_allUIndex[row], m_allUIndex[column]) += value;
+  }
+
+  void matrixSetValue(DoFLocalId row, DoFLocalId column, Real value) override
+  {
+    //m_csr_view.values()[indexValue(row, column)] = value;
+    (*m_builder)(m_allUIndex[row], m_allUIndex[column]) = value;
+
+  }
+
+  CSRFormatView& getCSRValues() override { return m_csr_view; };
+
+  void setCSRValues(const CSRFormatView& csr_view) override
+  {
+    m_csr_view = csr_view;
+  }
+
+  bool hasSetCSRValues() const override { return true; }
+
+  void eliminateRow(DoFLocalId row, Real value) override
+  {
+    ARCANE_THROW(NotImplementedException, "");
+  }
+
+  void eliminateRowColumn(DoFLocalId row, Real value) override
+  {
+    ARCANE_THROW(NotImplementedException, "");
+  }
+
+  void applyMatrixTransformation() override;
+  void applyRHSTransformation() override;
+
+  void clearValues() override
+  {
+    info() << "[CsrImpl]: Clear values";
+    DoFLinearSystemImplBase::clearValues();
+    m_csr_view = {};
+    m_alien_bsr_format.reset() ;
+  }
+
+  void startSystemAssembly() {
+    m_alien_bsr_format->startSystemFillingStep() ;
+    m_builder = m_alien_bsr_format->getMatrixBuilder() ;
+    m_allUIndex = m_alien_bsr_format->getAllUIndex() ;
+  }
+
+  void endSystemAssembly() {
+    m_alien_bsr_format->endSystemFillingStep() ;
+  }
+
+  void solve() override;
+
+  void setSolverCommandLineArguments(const CommandLineArguments& args) override
+  {
+    info() << "[Alien-Info] initialize command lines arguments";
+    auto argv = *args.commandLineArgv();
+    auto o = info() << "[Alien-Info] ./" << argv[0];
+
+
+    for (int i = 1; i < *args.commandLineArgc(); i++)
+      o << ' ' << argv[i];
+  }
+
+  void setSolver(Alien::ILinearSolver* s) { m_solver_backend = s; }
+
+  CaseOptionsAlienDoFLinearSystemFactory* options = nullptr;
+
+ private:
+  void _setVectorsCpu(Alien::Vector& vectorX, Alien::Vector& vectorB) ;
+  void _getSolutionCpu(Alien::Vector& vectorX) ;
+#ifdef ALIEN_USE_SYCL
+  void _setVectorsAcc(Alien::Vector& vectorX, Alien::Vector& vectorB) ;
+  void _getSolutionAcc(Alien::Vector& vectorX) ;
+#endif
+
+  Alien::ILinearSolver* m_solver_backend = nullptr;
+  VariableDoFInt32 m_dof_matrix_numbering;
+
+  NumArray<Real, MDDim1> m_rhs_work_values;
+  //! Work array to store values of solution vector in parallel
+  NumArray<Real, MDDim1> m_result_work_values;
+
+  Alien::ProfiledMatrixBuilder* m_builder = nullptr;
+  std::unique_ptr<AlienBSRFormat> m_alien_bsr_format;
+  ConstArrayView<Arccore::Integer> m_allUIndex;
+
+  CSRFormatView m_csr_view;
+};
+
+} // namespace Arcane::FemUtils
---
+++ test/CAWF/MecaFEM/mecafem-cube.arc
@@ -0,0 +1,68 @@
+<?xml version="1.0"?>
+<case codename="ShArc" xml:lang="en" codeversion="1.0">
+  <arcane>
+    <title>Sample</title>
+    <timeloop>ElasticityLoop</timeloop>
+  </arcane>
+
+  <arcane-post-processing>
+   <output-period>1</output-period>
+   <output>
+     <variable>U</variable>
+     <variable>CellDivU</variable>
+     <variable>CellPressure</variable>
+   </output>
+  </arcane-post-processing>
+
+  <mesh>
+    <file internal-partition="true">meshes/cube.vt2</file>
+  </mesh>
+
+  <fem>
+    <hex-quad-mesh>true</hex-quad-mesh>
+    <E>200e9</E>
+    <nu>0.3</nu>
+    <f>-9.8e12, -7.5e12, 5.9e12</f>
+    <boundary-conditions>
+      <dirichlet>
+        <surface>YMIN</surface>
+        <value>NULL 0. NULL</value>
+      </dirichlet>
+      <dirichlet>
+        <surface>YMAX</surface>
+        <value>NULL 0. NULL</value>
+      </dirichlet>
+      <dirichlet>
+        <surface>XMIN</surface>
+        <value>0. NULL NULL</value>
+      </dirichlet>
+      <dirichlet>
+        <surface>XMAX</surface>
+        <value>0. NULL NULL</value>
+      </dirichlet>
+      <dirichlet>
+        <surface>ZMIN</surface>
+        <value>0. 0. 0.</value>
+      </dirichlet>
+    </boundary-conditions>
+
+    <max-iter>1</max-iter>
+    <matrix-format>Alien-BSR</matrix-format>
+    <linear-system name="AlienLinearSystem">
+      <linear-solver name="AlienCoreSolver">
+        <backend>SimpleCSR</backend>
+        <solver>BCGS</solver>
+        <preconditioner>Diag</preconditioner>
+        <max-iter>1000</max-iter>
+        <tol>1.e-6</tol>
+        <output-level>1</output-level>
+      </linear-solver>
+    </linear-system>
+    <dynamic-mesh-mng name="DynamicMeshMng">
+        <mesh>mesh/cube</mesh>
+        <format>vt2</format>
+        <first-event-id>1</first-event-id>
+    </dynamic-mesh-mng>
+    <geometry-mng name="Euclidian3Geometry"/>
+  </fem>
+</case>
