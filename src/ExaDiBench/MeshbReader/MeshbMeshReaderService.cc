34e4266de65e0481986d38f5ccee82846d28876a
aurelien dauteuil <ad282543@dedippcaa197.extra.cea.fr>
2026-05-01 15:26:15 +0200

    initial version of ouranos structure into sharc ones

--- src/ExaDiBench/MeshbReader/MeshbMeshReaderService.cc
+++ src/ExaDiBench/MeshbReader/MeshbMeshReaderService.cc
@@ -5,13 +5,54 @@
 // SPDX-License-Identifier: Apache-2.0
 //-----------------------------------------------------------------------------
 // -*- C++ -*-
+#include <arcane/ArcaneVersion.h>
+#include <arcane/utils/ArcanePrecomp.h>
 
-#include "arcane/BasicService.h"
-#include "arcane/IMeshReader.h"
-#include "arcane/FactoryService.h"
+#include <arcane/utils/Iostream.h>
+#include <arcane/utils/StdHeader.h>
+#include <arcane/utils/HashTableMap.h>
+#include <arcane/utils/ValueConvert.h>
+#include <arcane/utils/ScopedPtr.h>
+#include <arcane/utils/ITraceMng.h>
+#include <arcane/utils/String.h>
+#include <arcane/utils/StringBuilder.h>
+#include <arcane/utils/IOException.h>
+#include <arcane/utils/Collection.h>
+#include <arcane/utils/Enumerator.h>
+#include <arcane/utils/OStringStream.h>
+#include <arcane/ArcaneVersion.h>
+#include <arcane/FactoryService.h>
+#include <arcane/IMeshReader.h>
+#include <arcane/ISubDomain.h>
+#include <arcane/IMeshSubMeshTransition.h>
+#include <arcane/IItemFamily.h>
+#include <arcane/Item.h>
+#include <arcane/ItemEnumerator.h>
+#include <arcane/VariableTypes.h>
+#include <arcane/IParallelMng.h>
 
+#include <arcane/IIOMng.h>
+#include <arcane/IXmlDocumentHolder.h>
+#include <arcane/XmlNodeList.h>
+#include <arcane/XmlNode.h>
+
+#include <arcane/IMeshUtilities.h>
+#include <arcane/IMeshWriter.h>
+#include <arcane/BasicService.h>
+
+#include "arcane/utils/PlatformUtils.h"
+#include "arcane/utils/Convert.h"
+#include "arcane/utils/NotImplementedException.h"
+
+#include <arcane/utils/UserDataList.h>
+#include <arcane/utils/IUserData.h>
+#include <arcane/utils/AutoDestroyUserData.h>
+
+#include "ArcGeoSim/Utils/Utils.h"
 #include "ouranos/config.h"
 #include "ouranos/kernel/info.hpp"
+#include "ouranos/mesh/mesh.hpp"
+#include "ouranos/kernel/ouranos.hpp"
 
 using namespace Arcane;
 
@@ -28,12 +69,12 @@ virtual IMeshReader::eReturnType readMeshFromFile(IPrimaryMesh* mesh,
                                      const String& file_name,
                                      const String& dir_name,
                                      bool use_internal_partition);
+
 };
 
 MeshbMeshReader::MeshbMeshReader(const ServiceBuildInfo& sbi)
 : Arcane::BasicService(sbi)
 {
-
 }
 
 bool MeshbMeshReader::allowExtension(const String& str)
@@ -54,14 +95,72 @@ readMeshFromFile(IPrimaryMesh* mesh,
   info() << "=== READING Meshb MESH ===";
 
   info () << "Testing ouranos linking " << Ouranos::Kernel::version();
+  auto rns = std::make_shared<Ouranos::Kernel::Ouranos>();
+  Ouranos::Mesh::Mesh<3, Ouranos::Kernel::Block> msh(rns);
+  msh.read(file_name.localstr());
 
-  fatal() << "Meshb Reader not yet implemented";
+  const auto& vertices = msh.ver();
+  Integer nb_nodes = vertices.nbr();
 
-  // auto rns = std::make_shared<Ouranos::Kernel::Ouranos>(com());
-  // Ouranos::Mesh::Mesh<3, Ouranos::Kernel::Block> msh(rns);
-  // msh.read(file_name.c_str());
+  info() << "Ouranos version: " << Ouranos::Kernel::version();
+  info() << "Mesh read: " << nb_nodes << " nodes, "
+         << msh.tet().nbr() << " tetrahedra, "
+         << msh.tri().nbr() << " triangles";
+
+  // Arcane mesh
+  mesh->setDimension(3);
+
+  // Get tetra and triangles
+  const auto& tets = msh.tet();
+  Integer nb_tets = tets.nbr();
+  const auto& tris = msh.tri();
+  Integer nb_tris = tris.nbr();
+
+  // Build cells_infos array
+  SharedArray<Int64> cells_infos;
+  cells_infos.reserve(nb_tets * 6 + nb_tris * 5);
+
+  // Adding tetra
+  for (Integer i = 0; i < nb_tets; ++i) {
+      cells_infos.add(IT_Tetraedron4);
+      cells_infos.add(i);
+      for (Integer j = 0; j < 4; ++j) {
+          cells_infos.add(tets.con()[i + 1][j]);
+      }
+  }
+
+  // Adding triangles
+  for (Integer i = 0; i < nb_tris; ++i) {
+      cells_infos.add(IT_Triangle3);
+      cells_infos.add(nb_tets + i);
+      for (Integer j = 0; j < 3; ++j) {
+          cells_infos.add(tris.con()[i + 1][j]);
+      }
+  }
+
+  // Allocate cells
+  mesh->allocateCells(nb_tets + nb_tris, cells_infos, false);
+  mesh->endAllocate();
+
+  info() << "Arcane mesh created: " << mesh->nbNode() << " nodes, "
+         << mesh->nbCell() << " cells";
+
+  // Assign coords to nodes
+  const auto& coords = vertices.crd();
+  VariableNodeReal3& nodes_coord = mesh->nodesCoordinates();
+
+  ENUMERATE_NODE (inode, mesh->allNodes()) {
+      const Node& node = *inode;
+      Int64 uid = node.uniqueId().asInt64();
+      rns_real_t x = coords[uid - 1][0];
+      rns_real_t y = coords[uid - 1][1];
+      rns_real_t z = coords[uid - 1][2];
+      nodes_coord[inode] = Real3(x, y, z);
+  }
+  
   // auto refined_msh = Ouranos::Mesh::refine(rns, msh);
 
+  fatal() << "Meshb Reader not yet implemented";
   return RTOk;
 };
 
